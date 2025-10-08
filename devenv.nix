a61eba51829bf58800ad2cdf2678f5aea02b8aaa
Franklin Delehelle <franklin.delehelle@odena.eu>
2025-10-08 13:31:10 +0300

    build: add devenv

---
+++ devenv.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  cachix.enable = false;
+
+  packages = [ pkgs.git pkgs.tree-sitter pkgs.git-cliff pkgs.cargo-release ];
+
+  languages = {
+    rust.enable = true;
+    typescript.enable = true;
+  };
+
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    cargo-check.enable = true;
+  };
+}
