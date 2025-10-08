25663f7f76921d5a5d75a78eec19cd62b4378364
Antonin Delpeuch <antonin@delpeuch.eu>
2025-10-08 14:22:47 +0200

    feat: expose highlight query in Rust bindings

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
-import tree_sitter
+from tree_sitter import Language, Parser
 import tree_sitter_newick
 
 
 class TestLanguage(TestCase):
     def test_can_load_grammar(self):
         try:
-            tree_sitter.Language(tree_sitter_newick.language())
+            Parser(Language(tree_sitter_newick.language()))
         except Exception:
             self.fail("Error loading Newick grammar")
